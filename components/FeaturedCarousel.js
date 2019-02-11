6731bf723fb03615397df5f407313a66efc4773a
David Gonzalez <davidg@vimeo.com>
2019-02-10 22:57:27 -0500

    setup start of featured carousel

--- components/FeaturedCarousel.js
+++ components/FeaturedCarousel.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Slider from 'react-slick';
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 
 const FeaturedCarousel = props => {
-  return <div>{props.name}</div>;
+  return (
+    <Slider {...settings}>
+      {props._embedded.items.map(item => {
+        return (
+          <div>
+            <div>{item.name}</div>
+            <img src={item.thumbnail.medium} />
+          </div>
+        );
+      })}
+    </Slider>
+  );
 };
 
 export default FeaturedCarousel;
